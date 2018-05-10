99ef2bc5fbeb6b0aaeb5121ef31d5656960b393d
Jiri Rebenda <jiri.rebenda@htmldriven.com>
2018-05-11 01:40:13 +0200

    Release 1.2.1

--- src/Application.php
+++ src/Application.php
@@ -20,7 +20,7 @@ use RuntimeException;
 class Application
 {
     /** @var string */
-    const VERSION = '1.1.0';
+    const VERSION = '1.2.1';
 
     /** @var string */
     const DEFAULT_DOMAIN = 'cors-proxy.htmldriven.com';
